9ce21ff6621391dc570c8729c50f795f40d1f771
Alice Chu <chienyi.alice@gmail.com>
2024-02-28 11:19:50 +0800

    Hello RN

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>數位三甲111019015朱倩儀</Text>
       <StatusBar style="auto" />
     </View>
   );
